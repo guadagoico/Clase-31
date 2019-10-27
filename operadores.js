430cd6bd72b6a7883c52b063d0885ba41fd2d061
Guada <guadagoico@gmail.com>
2019-10-27 16:37:10 -0300

    subir practico

---
+++ operadores.js
@@ -0,0 +1,75 @@
+/** EJERCICIO 27 **/
+let numero1 = 5;
+let numero2 = 6;
+
+console.log("sumo 5 + 6 y da:", numero1 + numero2)
+
+console.log("resto 5 -6 y da:", numero1 - numero2)
+
+console.log("multiplico 5 * 6 y da:", numero1 * numero2)
+
+console.log("divido 5 / 6 y da:", numero1 / numero2)
+
+console.log("El resto de  5 % 6 y da:", numero1 % numero2)
+
+
+/** EJERCICIO 28 **/
+
+let numero= 9;
+
+console.log("La tabla del 9:" + " " + numero * 1, numero * 2, numero * 3, numero * 4, numero * 5, numero * 6, numero * 7, numero * 8, numero * 9, numero * 10);
+
+/** EJERCICIO 29 **/
+
+let cuadrado= 10;
+
+console.log("El perimetro del cuadrado es:" + " " + cuadrado*4);
+
+/** EJERCICIO 30 **/
+
+let lado=5;
+console.log("El area del cuadrado es:"  + " " + lado*2 );
+
+/** EJERCICIO 31 **/
+
+let lado1=5;
+
+let lado2=10;
+
+let lado3=20;
+
+console.log("El perimetro del triangulo es:", + lado1 + lado2 + lado3 );
+
+/** EJERCICIO 32 **/
+
+let altura=10;
+let base=4;
+
+console.log("El perímetro es:" + " " + altura + base + " " + "y el área es:" + " " + altura*base);
+
+
+
+
+/**EJERCICIO 33 **/
+
+let cantidadDePersonas =100
+cantidadDePersonas +=5
+console.log("Cantidad de Personas:" + " " + cantidadDePersonas);
+
+cantidadDePersonas -=3
+console.log("Cantidad de personas:" + " " + cantidadDePersonas);
+
+cantidadDePersonas *=2;
+console.log("Cantidad de personas:" + " " + cantidadDePersonas);
+
+
+/**EJERCICIO 35*/
+
+let numeroDeCliente=143245
+let numeroDeClienteIngresado=143245;
+
+console.log("El número ingresado por el cliente es correcto:", (numeroDeCliente == numeroDeClienteIngresado))
+
+console.log("El cliente ingresó un número de cliente válido:", (numeroDeCliente === numeroDeClienteIngresado))
+
+
